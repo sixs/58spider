f66b91721f85c38ea3f18ef35903c01bff7e2ce0
e <2557692481@qq.com>
2019-03-21 16:40:06 +0800

    config

---
+++ config.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+'''
+desc: 配置文件
+'''
+# 登录信息
+LOGIN_INFO = {
+	'username': 'xxx',
+	'password': 'xxx',
+}
+
+# 简历基本信息, mobile为注册的手机号（验证过的手机号）
+BASIC_INFO = {
+    'truename': 'xxx',
+    'sex':'false',
+    'age':21,
+    'education':4,
+    'workYears':0,
+    'mobile':'xxx',
+    'keyCode':'',
+    'pic':'',
+    'postfrom':5
+}
+
+COOKIES_PATH = './cookies.txt'
+ENCRYPT_PATH = './encrypt.js'
